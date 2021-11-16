fd0fa15fa9031d189e93c625a4a9b832f4d1844b
Katarzyna Orlinska <orlinska.kat@gmail.com>
2021-11-16 18:07:29 +0100

    create section with logo

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../img/logo.png";
 
 const Header = () => {
   return (
     <div className="header">
+      <img className="logo" alt="logo" src={logo}></img>
       <a href="#top">Home</a>
       <a href="#about">O mnie</a>
       <a href="#specs">Specjalizacje</a>
