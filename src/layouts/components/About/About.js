0afcc2016573d8789389235c45dd5b2807d30edb
Natalia Nowak <natalia.nowak1@gmail.com>
2021-12-09 19:52:03 +0100

    create homebox and update about component

--- src/layouts/components/About/About.js
+++ src/layouts/components/About/About.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./About.css";
+import about from '../../../img/about.jpg';
 
 const About = () => {
   return (
     <div className="about" id="about">
-      About
+      <h3>O mnie</h3>
+            <div className='aboutBox'>
+                <div className='aboutPhoto'>
+                    <img alt='Agnieszka Janda' src={about}></img>
+                </div>
+                <div className='aboutDesc'>
+                    <h2>Agnieszka JANDA<br/> <br/></h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum expedita veritatis in neque fugit, consequuntur, iste similique laboriosam quaerat eaque eveniet quos soluta illo doloremque tempora corrupti tenetur? Fuga!</p>
+                    <br/>
+                    <br/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum expedita veritatis in neque fugit, consequuntur, iste similique laboriosam quaerat eaque eveniet quos soluta illo doloremque tempora corrupti tenetur? Fuga!</p> 
+                    <br/>
+                    <br/>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint laborum expedita veritatis in neque fugit, consequuntur, iste similique laboriosam quaerat eaque eveniet quos soluta illo doloremque tempora corrupti tenetur? Fuga!</p>
+                </div>
+            </div>
     </div>
   );
 };
--- src/layouts/components/Home/Home.js
+++ src/layouts/components/Home/Home.js
@@ -3,7 +3,16 @@ import "./Home.css";
 
 const Home = () => {
     return(
-        <div className="home" id="home">Home</div>
+        <div className="home" id="home">
+            <div className="photo">
+                <div className="homeBox">
+                    <div className="homeBoxIn">
+                        <div className="homeTitle">Agnieszka Janda</div>
+                        <div className="homeText">Doświadczenie, bezpieczeństwo, współpraca <br></br>i profesjonalizm</div>
+                    </div>
+                </div>
+           </div>
+        </div>
     );
 };
 
